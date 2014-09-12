b7dfaa38f6e61c90ec423349d42a6df355922be2
milodky <yext4011@gmail.com>
2014-09-11 22:23:31 -0700

    personally I hate not using vim

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4b3d113c81f1a00b2577e9db52379c014714a0c00637c439d1e3a2c85f56bf8ee1977aa8e5bff9854732feea883249a8afa077bc7b206a897e2a2f4c905a7b9a'
